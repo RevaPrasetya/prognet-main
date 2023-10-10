9db1a955fb45af8a50a1f27295ec123e4a8abe38
RevaPrasetya <revabro3@gmail.com>
2023-10-11 00:19:05 +0800

    Tampilan Kartu Form

--- tugas/tugas-form-php/index.php
+++ tugas/tugas-form-php/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas Javascript - 2205551015</title>
+    <title>Tugas PHP Form - 2205551015</title>
 
     <link rel="shortcut icon" href="../../assets/icon/soft-star.svg" type="image/x-icon">
 
@@ -21,11 +21,18 @@
   
       <div class="row g-5">
         <div class="col-md-12 col-lg-12">
-          <form class="needs-validation" action="index.php" method="POST" novalidate>
+          <form class="needs-validation" action="kartu-mahasiswa.php" method="POST" novalidate>
             <div class="row g-3">
-                <div class="col-12">
+                <div class="col-6">
+                  <label for="gambar_profil" class="form-label">Link Gambar Profil</label>
+                  <input type="text" class="form-control" name="gambar_profil" id="gambar_profil" placeholder="http:// .jpg" required>
+                  <div class="invalid-feedback">
+                    Masukan gambar profil
+                  </div>
+                </div>
+                <div class="col-6">
                   <label for="nim" class="form-label">NIM</label>
-                  <input type="number" class="form-control" id="nim" placeholder="220*******" required>
+                  <input type="number" class="form-control" name="nim" id="nim" placeholder="220*******" required>
                   <div class="invalid-feedback">
                     Masukan Nomor Induk Mahasiswa anda.
                   </div>
@@ -33,7 +40,7 @@
 
               <div class="col-sm-6">
                 <label for="firstName" class="form-label">Nama Depan</label>
-                <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
+                <input type="text" class="form-control" id="firstName" name="first_name" placeholder="" value="" required>
                 <div class="invalid-feedback">
                   Masukan nama depan yang benar
                 </div>
@@ -41,7 +48,7 @@
   
               <div class="col-sm-6">
                 <label for="lastName" class="form-label">Nama Belakang</label>
-                <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
+                <input type="text" class="form-control" id="lastName" name="last_name" placeholder="" value="" required>
                 <div class="invalid-feedback">
                     Masukan nama belakang yang benar
                 </div>
@@ -66,7 +73,7 @@
 
               <div class="col-12">
                 <label for="email" class="form-label">Email </label>
-                <input type="email" class="form-control" id="email" placeholder="contoh@example.com" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="contoh@example.com" required>
                 <div class="invalid-feedback">
                     Masukan alamat email yang benar
                 </div>
@@ -74,7 +81,7 @@
   
               <div class="col-12">
                 <label for="address" class="form-label">Alamat Rumah</label>
-                <input type="text" class="form-control" id="address" placeholder="Jln Uluwatu" required>
+                <input type="text" class="form-control" id="address" name="alamat" placeholder="Jln Uluwatu" required>
                 <div class="invalid-feedback">
                     Masukan alamat tempat tinggal yang benar
                 </div>
@@ -82,7 +89,7 @@
   
               <div class="col-md-6">
                 <label for="faculty" class="form-label">Fakultas</label>
-                <select class="form-select" id="faculty" required>
+                <select class="form-select" id="faculty" name="fakultas" required>
                   <option value="">Pilih Fakultas</option>
                   <option>Teknik</option>
                   <option>MIPA</option>
@@ -97,7 +104,7 @@
   
               <div class="col-md-6">
                 <label for="prodi" class="form-label">Program Studi</label>
-                <select class="form-select" id="prodi" required>
+                <select class="form-select" id="prodi" name="prodi" required>
                   <option value="">Pilih Program Studi</option>
                   <option>Teknologi Informasi</option>
                   <option>Teknik Mesin</option>
@@ -110,55 +117,6 @@
                     Pilih program studi yang benar
                 </div>
               </div>
-            </div>
-
-            <hr class="my-4">
-
-            
-            <div class="col-12">
-                <label for="username" class="form-label">Username</label>
-                <div class="input-group has-validation">
-                    <span class="input-group-text">@</span>
-                    <input type="text" class="form-control" id="username" placeholder="Masukan username" required>
-                    <div class="invalid-feedback">
-                    Masukkan username yang benar
-                    </div>
-                </div>
-            </div>
-
-            <div class="row g-3 my-2">
-                
-                <div class="col-md-6">
-                    <label for="password" class="form-label">Password</label>
-                    <div class="input-group has-validation">
-                        <input type="password" class="form-control" id="password" placeholder="Masukan password" required>
-                        <div class="invalid-feedback">
-                        Masukkan password yang benar
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-md-6">
-                    <label for="confirm_password" class="form-label">Password</label>
-                    <div class="input-group has-validation">
-                        <input type="password" class="form-control" id="confirm_password" placeholder="Konfirmasi password anda" required>
-                        <div class="invalid-feedback">
-                            Konfirmasi password anda
-                        </div>
-                    </div>
-                </div>
-            </div>
-  
-            <hr class="my-4">
-  
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="news" required>
-              <label class="form-check-label" for="news">Beritahu berita penting melalui Email</label>
-            </div>
-  
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="save-info" required>
-              <label class="form-check-label" for="save-info">Simpan informasi untuk login selanjutnya</label>
             </div>
   
             <hr class="my-4">
@@ -175,6 +133,110 @@
   </div>
 
   <script src="../../lib/bootstrap/bootstrap.bundle.min.js"></script>
-  <script src="main.js"></script>
+  <script type="text/javascript">
+    window.addEventListener("load", function () {
+    // Mendapatkan element formnya
+    const form = document.querySelector("form.needs-validation");
+
+    form.addEventListener("submit", function (event) {
+      // Mencegah form untuk tersubmit terlebih dahulu
+      event.preventDefault();
+
+      // Jalankan fungsi Validasi
+      const isValid = validateForm();
+
+      // Jika fungsi validateForm() memberikan nilai true, maka isi form sudah valid
+      if (isValid) {
+        //Kode di bawah berguna untuk lanjut submit sperti biasa
+        form.submit();
+      }
+    });
+
+    function validateForm() {
+      // Melakukan reset ulang tampilan validasi di form
+      resetValidationStyles();
+
+      // Variabel sebagai patokan validasi form true atau false
+      let isValid = true;
+
+      // Validasi NIM
+      const nimInput = document.getElementById('nim');
+        if (!nimInput.checkValidity() || !isNumeric(nimInput.value)) {
+          isValid = false;
+          displayError(nimInput);
+        }
+
+      // Validasi First Name
+      const firstNameInput = document.getElementById("firstName");
+      if (!firstNameInput.checkValidity()) {
+        isValid = false;
+        displayError(firstNameInput);
+      }
+
+      // Validasi Last Name
+      const lastNameInput = document.getElementById("lastName");
+      if (!lastNameInput.checkValidity()) {
+        isValid = false;
+        displayError(lastNameInput);
+      }
+
+      // Validasi Email
+      const emailInput = document.getElementById("email");
+      if (!emailInput.checkValidity() || !isValidEmail(emailInput.value)) {
+        isValid = false;
+        displayError(emailInput);
+      }
+
+      // Validasi Address atau Alamat
+      const addressInput = document.getElementById("address");
+      if (!addressInput.checkValidity()) {
+        isValid = false;
+        displayError(addressInput);
+      }
+
+      // Validasi Faculty
+      const facultyInput = document.getElementById("faculty");
+      if (!facultyInput.checkValidity()) {
+        isValid = false;
+        displayError(facultyInput);
+      }
+
+      // Validasi Program Studi
+      const prodiInput = document.getElementById("prodi");
+      if (!prodiInput.checkValidity()) {
+        isValid = false;
+        displayError(prodiInput);
+      }
+
+      // Memberikan nilai validasi yang baru saja dilakukan, jika false maka proses submit tidak akan berjalan
+      return isValid;
+    }
+
+    function isValidEmail(email) {
+      //Menggunakan Regex untuk melakukan validasi email
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }
+
+    function isNumeric(value) {
+      //Untuk mengecek apabila benar nilainya berupa angka
+      return /^\d+$/.test(value);
+    }
+
+    function resetValidationStyles() {
+      //Mengapus kelas "is-invalid"
+      const invalidControls = document.querySelectorAll(".is-invalid");
+      invalidControls.forEach((control) => {
+        control.classList.remove("is-invalid");
+      });
+    }
+
+    function displayError(inputElement) {
+      //Menambahkan kelas "is-invalid" apabila terjadi error pada input
+      inputElement.classList.add("is-invalid");
+    }
+  });
+
+  </script>
 </body>
 </html>
---
+++ tugas/tugas-form-php/kartu-mahasiswa.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas PHP Form - 2205551015</title>
+
+    <link rel="shortcut icon" href="../../assets/icon/soft-star.svg" type="image/x-icon">
+
+    <link rel="stylesheet" href="../../lib/bootstrap/bootstrap.min.css">
+</head>
+<body>
+    
+<div class="container">
+    <main>
+      <div class="py-5 text-center">
+        <img class="d-block mx-auto mb-4" src="../../assets/icon/soft-star-red.svg" alt="" width="72" height="57">
+        <h2>Kartu Mahasiswa</h2>
+        <p class="lead">Hasil dari submit form</p>
+      </div>
+  
+      <div class="row g-5">
+        <?php
+            $nim = $_POST['nim'];
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $jenkel = $_POST['jenis_kelamin'];
+            $email = $_POST['email'];
+            $alamat = $_POST['alamat'];
+            $fakultas = $_POST['fakultas'];
+            $prodi = $_POST['prodi']
+        ?>
+        <div class="col-md-12 col-lg-12">
+            
+            <div class="card" style="border-radius: 15px;">
+                <div class="card-body p-4">
+                    <div class="d-flex text-black">
+                    <?php
+                        if(!empty($_POST['gambar_profil'])){
+
+                    ?>
+                    <div class="flex-shrink-0">
+                        <img src="<?php $gambar = $_POST['gambar_profil']; echo $gambar ?>"
+                        alt="Generic placeholder image" class="img-fluid"
+                        style="width: 180px; border-radius: 10px;">
+                    </div>
+                    <?php
+                        }
+                    ?>
+                    <div class="flex-grow-1 ms-3">
+                        <h5 class="mb-1">
+                            <?php
+                                echo $first_name;
+                            ?> 
+                            <span class="font-weight-bold">
+                                <?php
+                                    echo $last_name;
+                                ?> 
+                            </span>
+                        </h5>
+                        <p class="mb-2 pb-1" style="color: #2b2a2a;">
+                            <?php
+                                echo $jenkel;
+                            ?> 
+                        </p>
+                        <p class="mb-2 pb-1" style="color: #2b2a2a;">
+                            <?php
+                                echo $nim;
+                            ?> 
+                        </p>
+                        <p class="mb-2 pb-1" style="color: #2b2a2a;">
+                            <?php
+                                echo $email;
+                            ?> 
+                        </p>
+                        <div class="d-flex justify-content-start rounded-3 p-2 mb-2"
+                        style="background-color: #efefef;">
+                            <div>
+                                <p class="small text-muted mb-1">Fakultas</p>
+                                <p class="mb-0">
+                                    <p class="mb-2 pb-1" style="color: #2b2a2a;">
+                                        <?php
+                                            echo $fakultas;
+                                        ?> 
+                                    </p>
+                                </p>
+                            </div>
+                            <div class="px-3">
+                                <p class="small text-muted mb-1">Prodi</p>
+                                <p class="mb-0">
+                                    <p class="mb-2 pb-1" style="color: #2b2a2a;">
+                                        <?php
+                                            echo $prodi;
+                                        ?> 
+                                    </p>
+                                </p>
+                            </div>
+                        </div>
+                        <p class="mb-2 pb-1" style="color: #2b2a2a;">
+                            <?php
+                                echo $alamat;
+                            ?> 
+                        </p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+      </div>
+    </main>
+  
+    <footer class="my-5 pt-5 text-body-secondary text-center text-small">
+      <p class="mb-1">&copy; Reva Prasetya</p>
+    </footer>
+  </div>
+
+  <script src="../../lib/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+</html>
