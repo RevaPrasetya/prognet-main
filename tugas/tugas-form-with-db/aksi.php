2cf3d7d4eaa39af1d3f57848044d839704d35596
RevaPrasetya <revabro3@gmail.com>
2023-10-18 00:00:45 +0800

    test form (again)

--- tugas/tugas-form-with-db/aksi.php
+++ tugas/tugas-form-with-db/aksi.php
@@ -2,25 +2,25 @@
     require("koneksi.php");
 
     
-    // $nim = $_POST['nim'];
-    // $first_name = $_POST['first_name'];
-    // $last_name = $_POST['last_name'];
-    // $jenkel = $_POST['jenis_kelamin'];
-    // $email = $_POST['email'];
-    // $alamat = $_POST['alamat'];
-    // $fakultas = $_POST['fakultas'];
-    // $prodi = $_POST['prodi'];
-    // $gambar = $_POST['gambar_profil'];
+    $nim = $_POST['nim'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $jenkel = $_POST['jenis_kelamin'];
+    $email = $_POST['email'];
+    $alamat = $_POST['alamat'];
+    $fakultas = $_POST['fakultas'];
+    $prodi = $_POST['prodi'];
+    $gambar = $_POST['gambar_profil'];
 
-    // $data = mysqli_query($koneksi, "INSERT INTO data_mahasiswa VALUES ('', '$nim',  
-    // '$first_name', '$last_name', '$jenkel', '$email', '$alamat', '$fakultas', '$prodi','$gambar')");
+    $data = mysqli_query($koneksi, "INSERT INTO data_mahasiswa VALUES ('', '$nim',  
+     '$first_name', '$last_name', '$jenkel', '$email', '$alamat', '$fakultas', '$prodi','$gambar')");
 
     
-    // if(!$data){
-    echo mysqli_error($koneksi);
-    //header("location: kartu-mahasiswa.php?pesan=gagal");
-    // }else{
-    //     header("location: kartu-mahasiswa.php?pesan=sukses");
-    // }
+    if(!$data){
+        echo mysqli_error($koneksi);
+        //header("location: kartu-mahasiswa.php?pesan=gagal");
+    }else{
+        header("location: kartu-mahasiswa.php?pesan=sukses");
+    }
 
 ?>
