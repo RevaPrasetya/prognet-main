94c31d019a0ca6578dd2ebdfbe62195698717d3c
RevaPrasetya <revabro3@gmail.com>
2023-10-17 23:47:19 +0800

    Formulir dengan aksi ke database

---
+++ tugas/tugas-form-with-db/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+    $host     = "192.168.4.4";
+    $username = "2205551015";
+    $password = "2205551015";
+    $database = "db_2205551015";
+
+    $koneksi = mysqli_connect($host, $username, $password, $database) or die ("Gagal Koneksi")
+?>
