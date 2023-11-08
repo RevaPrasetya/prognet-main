e705625a7dde814f23fafcd437e286759e6a49cb
RevaPrasetya <revabro3@gmail.com>
2023-11-08 17:09:19 +0800

    Change everything to laravel only, (bye bye beautiful index file)

--- tugas/tugas-form-with-db/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host     = "192.168.4.4";
-    $username = "2205551015";
-    $password = "2205551015";
-    $database = "db_2205551015";
-
-    $koneksi = mysqli_connect($host, $username, $password, $database) or die ("Gagal Koneksi")
-?>
